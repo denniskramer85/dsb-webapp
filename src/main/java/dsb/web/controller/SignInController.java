2637275613a42929f8726b6720ec277356ec076e
Miel van Welzen <miel.van.welzen@hva.nl>
2020-09-01 19:10:41 +0200

    LoginBean gemaakt en geimplementeerd

--- src/main/java/dsb/web/controller/SignInController.java
+++ src/main/java/dsb/web/controller/SignInController.java
@@ -1,5 +1,6 @@
 package dsb.web.controller;
 
+import dsb.web.controller.beans.LoginBean;
 import dsb.web.domain.Customer;
 import dsb.web.repository.CustomerRepository;
 import dsb.web.service.SignInService;
@@ -7,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Controller
@@ -24,22 +27,22 @@ public class SignInController {
     }
 
     @GetMapping("sign-in")
-    public String signIn() {
+    public String signIn(Model model) {
+        model.addAttribute("loginBean", new LoginBean());
         return "sign-in";
     }
 
     @PostMapping("sign-in")
     public String signInHandler(
-            @RequestParam(name = "input_username") String username,
-            @RequestParam(name = "input_password") String password,
+            @ModelAttribute LoginBean loginBean,
             Model model) {
-        Customer loginCustomer = signInService.checkCredentials(username, password);
+        Customer loginCustomer = signInService.checkCredentials(loginBean.getUsername(), loginBean.getPassword());
         if (loginCustomer != null) {
             model.addAttribute("username", loginCustomer.getUsername());
             return "account_overview";
         } else {
-            model.addAttribute("username", username);
-            model.addAttribute("password", password);
+            model.addAttribute("username", loginBean.getUsername());
+            model.addAttribute("password", loginBean.getPassword());
             model.addAttribute("loginFailed", "true");
             return "sign-in";
         }
