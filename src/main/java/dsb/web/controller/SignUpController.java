9bbec6ba5cfe9cb9033c9611f4a259a4ba08cb75
Miel van Welzen <miel.van.welzen@hva.nl>
2020-09-03 22:45:56 +0200

    Bugfixes passwordcheck en account aanmaak

--- src/main/java/dsb/web/controller/SignUpController.java
+++ src/main/java/dsb/web/controller/SignUpController.java
@@ -2,15 +2,12 @@ package dsb.web.controller;
 
 import dsb.web.controller.beans.CustomerBean;
 import dsb.web.domain.Customer;
-import dsb.web.domain.Transaction;
 import dsb.web.repository.TransactionRepository;
 import dsb.web.service.SignupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 
 @Controller
@@ -42,7 +39,7 @@ public class SignUpController {
         model.addAttribute("customerBean", cb2);
 
         //return "sign-up";
-        return "postcode2";
+        return "sign-up";
     }
 
 
