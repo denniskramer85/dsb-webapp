0d04158e0c5fa32cf7b59e89ef3df018b2c5f5af
Sicco Koetsier <sicco.koetsier@hva.nl>
2020-09-03 12:27:37 +0200

    scriptje gelukt

---
+++ src/main/java/dsb/web/domain/TransactionDummy.java
@@ -0,0 +1,97 @@
+package dsb.web.domain;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class TransactionDummy {
+
+    @Id
+    @GeneratedValue
+    private int transactionID;
+
+    @ManyToOne()
+    private Account transactionAccountDebet;
+    @ManyToOne()
+    private Account transactionAccountCredit;
+
+    private double transactionAmount;
+    private String message;
+    private Timestamp transactionTimestamp;
+
+    public TransactionDummy(int transactionID, Account transactionAccountDebet, Account transactionAccountCredit, double transactionAmount, String message, Timestamp transactionTimestamp) {
+        this.transactionID = transactionID;
+        this.transactionAccountDebet = transactionAccountDebet;
+        this.transactionAccountCredit = transactionAccountCredit;
+        this.transactionAmount = transactionAmount;
+        this.message = message;
+        this.transactionTimestamp = transactionTimestamp;
+    }
+
+    public TransactionDummy(Account transactionAccountDebet, Account transactionAccountCredit, double transactionAmount, String message, Timestamp transactionTimestamp) {
+        this(0, transactionAccountDebet, transactionAccountCredit, transactionAmount, message, transactionTimestamp);
+    }
+
+    public TransactionDummy() {
+    }
+
+    @Override
+    public String toString() {
+        return "TransactionDummy{" +
+                "transactionID=" + transactionID +
+                ", transactionAccountDebet=" + transactionAccountDebet +
+                ", transactionAccountCredit=" + transactionAccountCredit +
+                ", transactionAmount=" + transactionAmount +
+                ", message='" + message + '\'' +
+                ", transactionTimestamp=" + transactionTimestamp +
+                '}';
+    }
+
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(int transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public Account getTransactionAccountDebet() {
+        return transactionAccountDebet;
+    }
+
+    public void setTransactionAccountDebet(Account transactionAccountDebet) {
+        this.transactionAccountDebet = transactionAccountDebet;
+    }
+
+    public Account getTransactionAccountCredit() {
+        return transactionAccountCredit;
+    }
+
+    public void setTransactionAccountCredit(Account transactionAccountCredit) {
+        this.transactionAccountCredit = transactionAccountCredit;
+    }
+
+    public double getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(double transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Timestamp getTransactionTimestamp() {
+        return transactionTimestamp;
+    }
+
+    public void setTransactionTimestamp(Timestamp transactionTimestamp) {
+        this.transactionTimestamp = transactionTimestamp;
+    }
+}
