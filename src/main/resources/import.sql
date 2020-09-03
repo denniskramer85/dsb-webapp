65ee0bdbfbb41463cfcda9a67a499cddcd3f23f8
DLGdkp <dlgoedkoop@gmail.com>
2020-09-03 14:03:35 +0200

    deleted old inserts from import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,21 +1,6 @@
 INSERT INTO `dsb`.`address` (`addressid`, `city`, `house_number`, `street`) VALUES ('11', 'Amsterdam', '1', 'Straat');
 INSERT INTO `dsb`.`customer` (`customerid`, `initials`, `password`, `social_security_no`, `surname`, `username`, `address_addressid`) VALUES ('11', 'D', 'dennis', '123456', 'Kramer', 'dennis', '11');
 
-
-INSERT INTO `dsb`.`account` (`dtype`, `accountid`, `account_no`, `balance`) VALUES ('ConsumerAccount', '20', 'NL40DSBB0123456789', '200');
-INSERT INTO `dsb`.`account` (`dtype`, `accountid`, `account_no`, `balance`) VALUES ('ConsumerAccount', '21', 'NL30DSBB0123456789', '300');
-INSERT INTO `dsb`.`account` (`dtype`, `accountid`, `account_no`, `balance`) VALUES ('ConsumerAccount', '22', 'NL20DSBB0123456789', '400');
-INSERT INTO `dsb`.`account` (`dtype`, `accountid`, `account_no`, `balance`) VALUES ('SMEAccount', '23', 'NL50DSBB0123456789', '10000');
-INSERT INTO `dsb`.`account` (`dtype`, `accountid`, `account_no`, `balance`) VALUES ('SMEAccount', '24', 'NL60DSBB0123456789', '20000');
-INSERT INTO `dsb`.`account` (`dtype`, `accountid`, `account_no`, `balance`) VALUES ('SMEAccount', '25', 'NL70DSBB0123456789', '15000');
-
-INSERT INTO `dsb`.`customer_accounts` (`holders_customerid`, `accounts_accountid`) VALUES (11, 20);
-INSERT INTO `dsb`.`customer_accounts` (`holders_customerid`, `accounts_accountid`) VALUES (11, 21);
-INSERT INTO `dsb`.`customer_accounts` (`holders_customerid`, `accounts_accountid`) VALUES (11, 22);
-INSERT INTO `dsb`.`customer_accounts` (`holders_customerid`, `accounts_accountid`) VALUES (11, 23);
-INSERT INTO `dsb`.`customer_accounts` (`holders_customerid`, `accounts_accountid`) VALUES (11, 24);
-INSERT INTO `dsb`.`customer_accounts` (`holders_customerid`, `accounts_accountid`) VALUES (11, 25);
-
 INSERT INTO `dsb`.`transaction_dummy` (`transactionid`, `message`, `transaction_amount`, `transaction_timestamp`, transaction_account_credit_accountid, transaction_account_debet_accountid) VALUES ('300', 'tr1', 100, '2020-01-01 01:01:01', 20, 21);
 INSERT INTO `dsb`.`transaction_dummy` (`transactionid`, `message`, `transaction_amount`, `transaction_timestamp`, transaction_account_credit_accountid, transaction_account_debet_accountid) VALUES ('301', 'tr2', 100, '2020-01-02 01:01:01', 20, 22);
 INSERT INTO `dsb`.`transaction_dummy` (`transactionid`, `message`, `transaction_amount`, `transaction_timestamp`, transaction_account_credit_accountid, transaction_account_debet_accountid) VALUES ('302', 'tr3', 100, '2020-01-03 01:01:01', 20, 23);
