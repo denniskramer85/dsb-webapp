e4bbc0a03c0b4ec05ee43017c81e96bf3af2e539
Miel van Welzen <miel.van.welzen@hva.nl>
2020-09-03 14:04:41 +0200

    Logger toegevoegd

--- src/main/java/dsb/web/controller/AccountOverviewController.java
+++ src/main/java/dsb/web/controller/AccountOverviewController.java
@@ -3,6 +3,8 @@ package dsb.web.controller;
 import dsb.web.domain.ConsumerAccount;
 import dsb.web.domain.Customer;
 import dsb.web.service.AccountOverviewService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,7 @@ import java.util.List;
 @SessionAttributes(AttributeMapping.LOGGED_IN_CUSTOMER)
 public class AccountOverviewController {
     private AccountOverviewService accountOverviewService;
+    private Logger logger = LoggerFactory.getLogger(AccountOverviewController.class);
 
     @Autowired
     public AccountOverviewController(AccountOverviewService accountOverviewService) {
@@ -28,7 +31,7 @@ public class AccountOverviewController {
     @GetMapping("account_overview")
     public String accountOverview(@ModelAttribute(AttributeMapping.LOGGED_IN_CUSTOMER) Customer loggedInCustomer, Model model) {
         List<ConsumerAccount> consumerAccountList = accountOverviewService.getConsumerAccountsForCustomer(loggedInCustomer);
-        System.out.println(consumerAccountList);
+        logger.debug(consumerAccountList.toString());
         return "account_overview";
     }
 }
