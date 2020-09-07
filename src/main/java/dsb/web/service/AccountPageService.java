16d658bb3de446688ff1aaaf66e919875c010644
Sicco Koetsier <sicco.koetsier@hva.nl>
2020-09-07 12:57:04 +0200

    ideeen voor tonen holders

--- src/main/java/dsb/web/service/AccountPageService.java
+++ src/main/java/dsb/web/service/AccountPageService.java
@@ -4,14 +4,15 @@ package dsb.web.service;
 import dsb.web.controller.beans.AccountPageBean;
 import dsb.web.domain.Account;
 import dsb.web.domain.Customer;
-import dsb.web.domain.SMEAccount;
 import dsb.web.domain.Transaction;
 import dsb.web.repository.TransactionRepository;
+import dsb.web.service.comparators.ComparatorHoldersBySurname;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -20,6 +21,9 @@ public class AccountPageService {
 
     private TransactionRepository transactionRepository;
 
+    /**max numbe rof holders shown**/
+    private int maxNrHoldersShown = 3;
+
     @Autowired
     public AccountPageService(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
@@ -27,19 +31,20 @@ public class AccountPageService {
 
     public AccountPageBean makeAccountPageBean(Account account) {
 
+        /**Achtereenvolgens beangegevens ophalen**/
         String typeAccount = account.printClassName();
         String accountNo = account.getAccountNo();
 
-        //TODO: nog via SMEA ophalen, nu hardcoded
-        String companyName = "HVA BV";
+        //TODO implementeren
+        String companyName = "HVA industries BV";
+//            if (account instanceof SMEAccount) {
+//                String compNameTemp = ((SMEAccount) account).getCompany().getName();
+//                System.out.println(compNameTemp);
+//                if (compNameTemp != null) {companyName = compNameTemp;}
+//            }
+//        System.out.println(companyName);
 
-//        if (account instanceof SMEAccount) {
-//            companyName = ((SMEAccount) account).getCompany().getName();
-//        }
-
-
-
-
+        //TODO hier nog string van maken (detail)
         List<String> holderNames = createListHolderNames(account.getHolders());
         String balance = String.format("%.2f", account.getBalance());
         String currentTime = getCurrentTime();
@@ -51,8 +56,12 @@ public class AccountPageService {
     }
 
 
-    //TODO nog max en volgorde?
+    //TODO VERBETEREN: return een string.format met alle specs rond max, kommas en e.a.
     public List<String> createListHolderNames (List<Customer> listHolders) {
+
+        //TODO test of werkt bij meerdere (sorteren op achternaam)
+        Collections.sort(listHolders, new ComparatorHoldersBySurname());
+
         List<String> holderNames = new ArrayList<>();
         String inits, inserts, surname, result;
         for (Customer c : listHolders) {
@@ -64,6 +73,13 @@ public class AccountPageService {
             result = String.format("%s %s%s", inits, inserts, surname);
             holderNames.add(result);
         }
+
+        //TODO test of werkt bij meerdere
+        if (holderNames.size() > maxNrHoldersShown) {
+            holderNames = holderNames.subList(0, (maxNrHoldersShown-1));
+            holderNames.add("e.a.");
+        }
+
         return holderNames;
     }
 
