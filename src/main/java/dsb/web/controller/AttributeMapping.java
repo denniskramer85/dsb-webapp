d43c5a07654aae5ac5f3c977e1400ec440f375e6
Dennis Kramer <denniskramer@mbp-van-dennis.home>
2020-09-09 17:48:32 +0200

    Employee, EmployeeSignIn Controller Service Repo en HTML + Bean

--- src/main/java/dsb/web/controller/AttributeMapping.java
+++ src/main/java/dsb/web/controller/AttributeMapping.java
@@ -3,4 +3,7 @@ package dsb.web.controller;
 public class AttributeMapping {
     // Logged in customer from session
     public final static String LOGGED_IN_CUSTOMER = "loggedInCustomer";
+
+    // Logged in employee from session
+    public final static String LOGGED_IN_EMPLOYEE = "loggedInEmployee";
 }
