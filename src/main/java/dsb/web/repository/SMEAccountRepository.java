8fc2c7604eefbdfa451322ff6ff852c18ac6cf72
Thijs Rodenburg <thijs.rodenburg@hva.nl>
2020-09-11 12:35:20 +0200

    update Pageable, not working

--- src/main/java/dsb/web/repository/SMEAccountRepository.java
+++ src/main/java/dsb/web/repository/SMEAccountRepository.java
@@ -2,13 +2,23 @@ package dsb.web.repository;
 
 import dsb.web.domain.Customer;
 import dsb.web.domain.SMEAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
-public interface SMEAccountRepository extends CrudRepository<SMEAccount, Integer> {
+public interface SMEAccountRepository extends JpaRepository<SMEAccount, Integer> {
     List<SMEAccount> findAllByHolders(Customer customer);
     List<SMEAccount> findAll();
+    List<SMEAccount> findTop10SaldoAccounts(Pageable pageable);
+
 }
+
+    //    @Query(value = "SELECT * FROM dsb.account WHERE balance = ?1 ORDER BY balance DESC LIMIT 10;",
+//            nativeQuery = true)
+
+//  replace crudrepository for jpa repo.
--- src/main/java/dsb/web/service/SmeDashboardService.java
+++ src/main/java/dsb/web/service/SmeDashboardService.java
@@ -8,8 +8,11 @@ import dsb.web.domain.Transaction;
 import dsb.web.repository.SMEAccountRepository;
 import dsb.web.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Service
@@ -27,12 +30,15 @@ public class SmeDashboardService {
     }
 
     public List<Transaction> getTop10SmeTransaction() {
+
         List<Transaction> transactionList = transactionRepository.findAll();
         return transactionList;
     }
 
+
     public List<SMEAccount> getTop10SmeBalance() {
-        List<SMEAccount> smeAccountsList = smeAccountRepository.findAll();
+        List<SMEAccount> smeAccountsList = smeAccountRepository.findTop10SaldoAccounts((Pageable) PageRequest.of( 0, 2));
+        System.out.println(smeAccountsList);
         return smeAccountsList;
     }
 
