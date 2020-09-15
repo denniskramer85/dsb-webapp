3c3ed5e179dc6c00ea700240031e4607827b02f7
Linda Haest <linda.haest@hva.nl>
2020-09-15 17:25:34 +0200

    aanvragen pinautomaat token wegschrijven wekrt

---
+++ src/main/java/dsb/web/controller/RequestPaymentMachineController.java
@@ -0,0 +1,34 @@
+package dsb.web.controller;
+
+import dsb.web.domain.SMEAccount;
+import dsb.web.domain.TokenPaymentMachine;
+import dsb.web.service.RequestPaymentMachineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes(AttributeMapping.SELECTED_ACCOUNT)
+public class RequestPaymentMachineController {
+
+    @Autowired
+    private RequestPaymentMachineService requestPaymentMachineService;
+
+
+    public RequestPaymentMachineController(RequestPaymentMachineService requestPaymentMachineService) {
+        super();
+        this.requestPaymentMachineService = requestPaymentMachineService;
+
+    }
+
+    @GetMapping("request-paymentmachine")
+    public String requestPaymentMachine(Model model, @ModelAttribute(AttributeMapping.SELECTED_ACCOUNT) SMEAccount smeAccount) {
+       TokenPaymentMachine token = requestPaymentMachineService.createAndSaveToken(smeAccount);
+        System.out.println(token);
+        return "confirm_paymentmachine_request";
+    }
+}
---
+++ src/main/java/dsb/web/domain/TokenPaymentMachine.java
@@ -0,0 +1,54 @@
+package dsb.web.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class TokenPaymentMachine {
+
+    @Id
+    @GeneratedValue
+    private int tokenID;
+
+    @ManyToOne
+    private SMEAccount smeAccount;
+    private String securityCode;
+
+    public TokenPaymentMachine(int tokenID, SMEAccount smeAccount) {
+        this.tokenID = tokenID;
+        this.smeAccount = smeAccount;
+    }
+
+    public TokenPaymentMachine(SMEAccount smeAccount) {
+        this.smeAccount = smeAccount;
+    }
+
+    public TokenPaymentMachine() {
+
+    }
+
+    public int getTokenID() {
+        return tokenID;
+    }
+
+
+    public void setTokenID(int tokenID) {
+        this.tokenID = tokenID;
+    }
+
+    public SMEAccount getSmeAccount() {
+        return smeAccount;
+    }
+
+    public void setSmeAccount(SMEAccount smeAccount) {
+        this.smeAccount = smeAccount;
+    }
+
+    @Override
+    public String toString() {
+        return "TokenPaymentMachine{" +
+                "tokenID=" + tokenID +
+                ", smeAccount=" + smeAccount +
+                ", securityCode=" + securityCode +
+                '}';
+    }
+}
