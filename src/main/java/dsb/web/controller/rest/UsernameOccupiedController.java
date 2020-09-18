26baeed93afbbbcdaebbd8e5060c1a574b48da07
Sicco Koetsier <sicco.koetsier@hva.nl>
2020-09-18 18:19:42 +0200

    na JS werk, werkt

---
+++ src/main/java/dsb/web/controller/rest/UsernameOccupiedController.java
@@ -0,0 +1,40 @@
+package dsb.web.controller.rest;
+
+
+import dsb.web.controller.beans.SignUpBean;
+import dsb.web.controller.beans.TransferNameBean;
+import dsb.web.domain.Account;
+import dsb.web.domain.Customer;
+import dsb.web.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/username-occupied-check")
+public class UsernameOccupiedController {
+
+    CustomerRepository customerRepository;
+
+    @Autowired
+    public UsernameOccupiedController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping(value = "/{username}")
+    public Boolean usernameExists(@PathVariable("username") String username) {
+
+        List<Customer> customer = customerRepository.findAllByUsername(username);
+
+        System.out.println(customer);
+        System.out.println(customer.size() == 0);
+
+        if (customer.size() == 0) {
+            return Boolean.FALSE;
+        } else return Boolean.TRUE;
+    }
+
+
+}
