e01f557980f67b9d53b0a6e41c418134fbdb2a55
Miel van Welzen <miel.van.welzen@hva.nl>
2020-09-24 20:36:25 +0200

    Begin testdata

--- src/main/java/dsb/web/WebApplicationSetup.java
+++ src/main/java/dsb/web/WebApplicationSetup.java
@@ -1,13 +1,19 @@
 package dsb.web;
 
+import dsb.web.controller.AccountOverviewController;
 import dsb.web.service.ApplicationStartupService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class WebApplicationSetup implements ApplicationListener<ApplicationReadyEvent> {
     ApplicationStartupService applicationStartupService;
+    private Logger logger = LoggerFactory.getLogger(AccountOverviewController.class);
 
     public WebApplicationSetup(ApplicationStartupService applicationStartupService) {
         this.applicationStartupService = applicationStartupService;
@@ -15,6 +21,13 @@ public class WebApplicationSetup implements ApplicationListener<ApplicationReady
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        try {
+            applicationStartupService.createCustomers();
+        } catch (IOException e) {
+            logger.debug(e.toString());
+        }
+
+
         applicationStartupService.setupBalances();
     }
 }
--- src/main/java/dsb/web/service/ApplicationStartupService.java
+++ src/main/java/dsb/web/service/ApplicationStartupService.java
@@ -1,14 +1,17 @@
 package dsb.web.service;
 
-import dsb.web.domain.Account;
-import dsb.web.domain.ConsumerAccount;
-import dsb.web.domain.SMEAccount;
+import dsb.web.domain.*;
 import dsb.web.repository.AccountRepository;
 import dsb.web.repository.ConsumerAccountRepository;
+import dsb.web.repository.CustomerRepository;
 import dsb.web.repository.SMEAccountRepository;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 @Service
 public class ApplicationStartupService {
@@ -16,12 +19,16 @@ public class ApplicationStartupService {
     private ConsumerAccountRepository consumerAccountRepository;
     private SMEAccountRepository smeAccountRepository;
     private TransactionService transactionService;
+    private CustomerRepository customerRepository;
+    private NewAccountService newAccountService;
 
-    public ApplicationStartupService(AccountRepository accountRepository, ConsumerAccountRepository consumerAccountRepository, SMEAccountRepository smeAccountRepository, TransactionService transactionService) {
+    public ApplicationStartupService(AccountRepository accountRepository, ConsumerAccountRepository consumerAccountRepository, SMEAccountRepository smeAccountRepository, TransactionService transactionService, CustomerRepository customerRepository, NewAccountService newAccountService) {
         this.accountRepository = accountRepository;
         this.consumerAccountRepository = consumerAccountRepository;
         this.smeAccountRepository = smeAccountRepository;
         this.transactionService = transactionService;
+        this.customerRepository = customerRepository;
+        this.newAccountService = newAccountService;
     }
 
     public void setupBalances() {
@@ -30,4 +37,39 @@ public class ApplicationStartupService {
             transactionService.updateBalance(account);
         }
     }
+
+    public void createCustomers() throws IOException {
+        File customers = new ClassPathResource("data/customer-data.csv").getFile();
+        String separator = ";";
+
+        try (Scanner scanner = new Scanner(customers)) {
+            while (scanner.hasNext()) {
+                String[] data = scanner.nextLine().split(separator);
+                // Create address
+                Address address = new Address(data[0], Integer.valueOf(data[1]), data [2], data[3], data[4].trim());
+
+                // Create customer and persist in database
+                Customer customer = new Customer(Integer.valueOf(data[10]), data[5], data[6], data[7], address, data[8], data[9], null);
+                customerRepository.save(customer);
+
+                // Create random amount of new accounts for customer
+                for (int i = 0; i < createRandomInteger(3); i++) {
+                    ConsumerAccount account = (ConsumerAccount) newAccountService.saveNewConsumerAccount(customer);
+                    // Do initial transaction for account
+                    transactionService.doInitialTransaction(account);
+                }
+
+                System.out.println("Klantnummer: " + customer.getUserID());
+
+
+            }
+        }
+    }
+
+    private int createRandomInteger(int bandwidth) {
+        int random = (int) (Math.random() * bandwidth) + 1;
+        return random;
+    }
+
+
 }
